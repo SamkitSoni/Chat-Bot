1cdd54630b03e20b559d43801975d472c46ec005
Samkit <samkitsoni09@gmail.com>
2024-07-17 00:59:19 +0530

    Added validator

--- backend/src/utils/vaidators.ts
+++ backend/src/utils/vaidators.ts
@@ -1,7 +1,35 @@
-import { body } from "express-validator";
+import { body , ValidationChain, validationResult} from "express-validator";
 
-const signupValidator = [
-    body("name").notEmpty().withMessage("Name is required"),
-    body("email").trim().isEmail().withMessage("Email is required"),
-    body("password").trim().isLength({min: 6}).notEmpty().withMessage("Password should contain atleast 6 characters")
+export const validate = (validations: ValidationChain[]) => {
+    return async (req, res, next) => {
+        for(let validation of validations) {
+            const result = await validation.run(req);
+            if(!result.isEmpty){
+                break;
+            }
+        }
+        const errors = validationResult(req);
+        if(errors.isEmpty()){
+            return next();
+        }
+        return res.status(422).json({errors: errors.array()});
+    };
+};
+
+export const loginValidator = [
+    body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Email is required"),
+    body("password")
+    .trim()
+    .isLength({min: 5})
+    .withMessage("Password should contain atleast 5 characters")
+]
+
+export const signupValidator = [
+    body("name")
+    .notEmpty()
+    .withMessage("Name is required"),
+    ...loginValidator,
 ]
